fc80cdb778a94f665735152248b56f64c6bccacf
majusiregar@gmail.com <47182263+bangcadel@users.noreply.github.com>
2021-11-24 16:33:36 +0700

    navigasi-angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,48 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class AppRoutingModule { }
+
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+const routes:Routes = [
+  {
+    path : 'login',
+    component : LoginComponent
+  },
+  {
+    path : 'register',
+    component : RegisterComponent
+  },
+  {
+    path : '',
+    pathMatch : 'full',
+    redirectTo : '/login'
+  },
+  {
+    path : 'admin',
+    loadChildren : () => import('./admin/admin.module').then(mod => mod.AdminModule)  
+  },
+  {
+    path: 'public',
+    loadChildren: () => import('./public/public.module').then(mod => mod.PublicModule)
+  }
+];
+
+@NgModule({
+  imports : [RouterModule.forRoot(routes)],
+  exports : [RouterModule]
+})
+
+export class AppRoutingModule {}
